25f1df5256dd60d447c5442eb5a6a08ceacc1500
Sheridans1262 <fed-ilya@bk.ru>
2021-03-29 21:06:57 +0300

    Simple c# ping-pong

---
+++ CSharpTest2/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace CSharpTest2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Background = new System.Windows.Forms.Panel();
+            this.GamePanel = new System.Windows.Forms.PictureBox();
+            this.GameBall = new System.Windows.Forms.PictureBox();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.Background.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GamePanel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GameBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Background
+            // 
+            this.Background.Controls.Add(this.GameBall);
+            this.Background.Controls.Add(this.GamePanel);
+            this.Background.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Background.Location = new System.Drawing.Point(0, 0);
+            this.Background.Name = "Background";
+            this.Background.Size = new System.Drawing.Size(694, 418);
+            this.Background.TabIndex = 0;
+            // 
+            // GamePanel
+            // 
+            this.GamePanel.BackColor = System.Drawing.Color.Red;
+            this.GamePanel.Location = new System.Drawing.Point(258, 363);
+            this.GamePanel.Name = "GamePanel";
+            this.GamePanel.Size = new System.Drawing.Size(200, 15);
+            this.GamePanel.TabIndex = 0;
+            this.GamePanel.TabStop = false;
+            // 
+            // GameBall
+            // 
+            this.GameBall.BackColor = System.Drawing.Color.Black;
+            this.GameBall.Location = new System.Drawing.Point(492, 221);
+            this.GameBall.Name = "GameBall";
+            this.GameBall.Size = new System.Drawing.Size(25, 25);
+            this.GameBall.TabIndex = 1;
+            this.GameBall.TabStop = false;
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 418);
+            this.Controls.Add(this.Background);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.Background.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.GamePanel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GameBall)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Background;
+        private System.Windows.Forms.PictureBox GameBall;
+        private System.Windows.Forms.PictureBox GamePanel;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ CSharpTest2/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpTest2
+{
+    public partial class Form1 : Form
+    {
+        private int speed_vert = -6;
+        private int speed_horz = -6;
+        public Form1()
+        {
+            InitializeComponent();
+
+            Timer.Enabled = true;
+
+            Cursor.Hide();
+
+            //this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+
+            GamePanel.Location = new Point(Background.Right/2 - GamePanel.Width/2,
+                                           Background.Bottom - Background.Bottom/10);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+
+            if (e.KeyCode == Keys.D)
+            {
+                Cursor.Position = new Point(Cursor.Position.X + 25,
+                                            Cursor.Position.Y);
+            }
+
+            if (e.KeyCode == Keys.A)
+            {
+                Cursor.Position = new Point(Cursor.Position.X - 25,
+                                            Cursor.Position.Y);
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (Cursor.Position.X < Background.Right - GamePanel.Width)
+            {
+                GamePanel.Left = Cursor.Position.X;
+            }
+
+            GameBall.Left += speed_horz;
+            GameBall.Top += speed_vert;
+
+            if (GameBall.Left <= Background.Left || GameBall.Right >= Background.Right)
+            {
+                speed_horz *= -1;
+            }
+
+            if (GameBall.Top <= Background.Top)
+            {
+                speed_vert *= -1;
+            }
+
+            if (GameBall.Bottom >= Background.Bottom)
+            {
+                Timer.Enabled = false;
+            }
+
+            if(GameBall.Bottom >= GamePanel.Top &&
+               GameBall.Left >= GamePanel.Left &&
+               GameBall.Right <= GamePanel.Right)
+            {
+                speed_vert *= -1;
+                /*speed_vert += 2;
+                speed_horz += 2;*/
+            }
+        }
+    }
+}
